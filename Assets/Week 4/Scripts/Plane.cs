7366b6de052e948d083ba581ce0ea11c519c0510
AndrewWilliams02 <willi365@sheridancollege.ca>
2024-01-31 14:39:42 -0500

    [TASK-2] Add plane movement - Add plane movement following mouse trail - Delete mouse trail passed by plane

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -8,12 +8,47 @@ public class Plane : MonoBehaviour
     public float newPointThreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Rigidbody2D rigidBody;
+    Vector2 currentPosition;
+    public float speed = 1;
 
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if (points.Count > 0 )
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rigidBody.rotation = -angle;
+        }
+        rigidBody.MovePosition(rigidBody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        lineRenderer.SetPosition(0, currentPosition);
+        if(points.Count > 0 )
+        {
+            if (Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+
+                for (int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i+1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
     }
 
     void OnMouseDown()
