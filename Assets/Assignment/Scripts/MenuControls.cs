d9ec345c59d8e7a5688b390b9822495745a68eb3
AndrewWilliams02 <willi365@sheridancollege.ca>
2024-02-19 23:54:53 -0500

    [TASK-1] Add Menu & Scenes - Add menu scene - Add UI to menu scene - Add button functionality - Create script to move between scenes

---
+++ Assets/Assignment/Scripts/MenuControls.cs
@@ -0,0 +1,21 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+    public void loadGame()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void exitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
